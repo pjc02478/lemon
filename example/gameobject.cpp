4d47e820d21bfb4194da75911b379f1b7bf6ecf7
pjc02478 <pjc0247@gmail.com>
2015-04-23 00:26:31 +0900

    Update gameobject.cpp

--- example/gameobject.cpp
+++ example/gameobject.cpp
@@ -21,7 +21,7 @@ void game_object::update(){
   printf("on update\n");
 }
 
-void game_object(const time::unit &duration, float _x,float _y){
+void game_object::move_to(const time::unit &duration, float _x,float _y){
   int iters = duration.to_ms() / frame<60>(1).to_ms();
   float step_x = x - _x;
   float step_y = y - _y;
