0af82d881e841c6ab44353178623f88c88033129
pjc02478 <pjc0247@gmail.com>
2015-04-28 01:40:39 +0900

    repeat 대충

---
+++ src/lemon/flowcontrol/repeat.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <stack>
+
+#include "../microthread/task.h"
+
+namespace lemon{
+	namespace flowcontrol{
+
+		/*
+		repeat(f, n){
+			n.times {
+				f();
+			}
+		}
+		repeat_until(f, s){
+			???
+		}
+		*/
+		
+	};
+};
