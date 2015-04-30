8ed73bcedfb8234e02cb25ad110d5c9e8bc7dddc
pjc02478 <pjc0247@gmail.com>
2015-05-01 02:11:26 +0900

    core 추가

---
+++ src/lemon/core/win32/core_win32.cpp
@@ -0,0 +1,14 @@
+﻿#include "../../lemon.h"
+
+#include <Windows.h>
+
+namespace lemon{
+	
+	bool initialize(){
+		return 
+			ConvertThreadToFiber(0) != nullptr ? true : false;
+	}
+	void quit(){
+		ConvertFiberToThread();
+	}
+};
