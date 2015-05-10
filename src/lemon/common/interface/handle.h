4d18518885ad073c6efe4b61746e6405e965f2e3
pjc02478 <pjc0247@gmail.com>
2015-05-10 21:41:50 +0900

    handle 새버전

--- src/lemon/common/interface/handle.h
+++ src/lemon/common/interface/handle.h
@@ -3,9 +3,25 @@
 namespace lemon{
 	namespace intf{
 
-		class handle{
-		public:
+		template<typename T, typename DEFAULT>
+		struct get_id_type{
+			template<typename Arg>
+			static auto Fn(int)
+				->decltype(T::id());
+			template<typename Arg>
+			static auto Fn(...)
+				->DEFAULT;
 
+			using type = decltype(Fn<T>(0));
+		};
+
+		//template <typename T>
+		class handle{
+			//using id_t =
+			//	typename get_id_type<T, unsigned int>::type;
+
+		public:
+			//virtual id_t get_id() const =0;
 			virtual unsigned int get_id() const =0;
 			virtual void join() const =0;
 		};
