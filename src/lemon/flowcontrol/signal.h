08981ec9574d259c5f09c1d9cb87216f11021e34
pjc02478 <pjc0247@gmail.com>
2015-06-10 13:55:04 +0900

    add_waiting_context를 protected로

--- src/lemon/flowcontrol/signal.h
+++ src/lemon/flowcontrol/signal.h
@@ -10,12 +10,14 @@ namespace lemon{
 		class signal{
 		public:
 			void wait();
-			void add_waiting_context(const microthread::task &ctx);
 
 			/* 대기하고 있는 context중 하나를 꺠웁니다 */
 			bool notify_one();
 			/* 대기하고 있는 모든 context를 깨웁니다 */
 			unsigned int notify_all();
+
+		protected:
+			void add_waiting_context(const microthread::task &ctx);
 
 		private:
 			std::stack<std::reference_wrapper<const microthread::task>> ctxs;
