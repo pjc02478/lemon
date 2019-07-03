e03c7501e7daff8c97053a6c0cf073badd428c10
Park June Chul <pjc0247@naver.com>
2019-07-03 17:05:00 +0900

    Update physics.cpp

--- example/physics.cpp
+++ example/physics.cpp
@@ -1,13 +1,13 @@
 void process_physics(){
   int count = 0;
   for(auto obj : objs){
-    /* 물리 계산
+    /* long-running physics calculations
      * 충돌 검사, 힘 계산 등...
      */
     
     count ++;
     
-    /* 남은 작업은 다음 프레임에 처리한다 */
+    /* Defers remaning tasks to next frame */
     if(count % 10 == 0)
       flowcontrol::delay();
   }
